5fd9b6e5ab8abd4e1bd57791932e80cfa57d2417
Otavio_Vittorassi <otavio.vittorassi@gmail.com>
2022-06-30 00:27:06 -0300

    Reescrito header com animação de seleção de item

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,20 +1,86 @@
-<script lang="coffee">
-    import { Link } from 'svelte-navigator'
+<script>
+    import { useNavigate } from 'svelte-navigator';
+
+    const navigate = useNavigate();
+
+    let activeMenu = 'home';
 </script>
 
-<header>
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="about">About</Link>
-  </nav>
+<header class="">
+  <div class="menu-nav-header">
+    
+    <div
+      class="{ activeMenu === 'home' ? 'nav-item active' : 'nav-item' }"
+      on:click="{
+        () => {
+          activeMenu = 'home';
+          navigate('/', { state: {} });
+        }
+      }"
+    >
+      Home
+    </div>
+
+    <div
+      class="{ activeMenu === 'about' ? 'nav-item active' : 'nav-item' }"
+      on:click="{
+        () => {
+          activeMenu = 'about';
+          navigate('about', { state: {} });
+        }
+      }"
+    >
+      About
+    </div>
+    
+  </div>
 </header>
 
-<style>
+<style lang="scss">
   header {
     background: #eeeeee;
     width: 100%;
-    margin: 20px auto;
-    padding: 20px;
-    max-width: 1024px;
+
+    & .menu-nav-header {
+      display: flex;
+      justify-content: space-evenly;
+
+      & .nav-item {
+        width: 100%;
+        padding: 20px 0;
+        text-align: center;
+        transition: 0.3s;
+        transition-timing-function: ease-out;
+        
+        &:hover {
+          cursor: pointer;
+          background: #dddddd;
+          transition: 0.3s;
+          transition-timing-function: ease-in;
+        }
+
+        &::after {
+          display:block;
+          content: '';
+          border-bottom: solid 3px #019fb6;
+          transform: scaleX(0) translateY(20px);
+          transition: transform 250ms ease-in-out;
+        }
+
+        &.active{
+          &:after {
+            transform: scaleX(1) translateY(20px);
+          }
+        }
+
+        & :global(a) {
+          text-decoration: none;
+        }
+
+        & p {
+          margin: 0;
+        }
+      }
+    }
   }
 </style>
